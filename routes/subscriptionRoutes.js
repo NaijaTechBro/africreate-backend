fc9b64f8f0b85f3125b8c35ffdcb562ec347a28a
NaijaTechBro <bakisodiq@gmail.com>
2025-04-13 19:49:37 +0100

    update -- subscription route

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -4,15 +4,24 @@ const subscriptionController = require('../controllers/subscriptionController');
 const { isAuthenticatedUser } = require('../middleware/authMiddleware');
 
 // Get user's subscriptions (protected route)
-router.get('/user', isAuthenticatedUser, subscriptionController.getUserSubscriptions);
+router.get('/user/subscriptions', isAuthenticatedUser, subscriptionController.getUserSubscriptions);
 
 // Get creator's subscribers (protected route)
-router.get('/creator', isAuthenticatedUser, subscriptionController.getCreatorSubscriptions);
+router.get('/creator/subscribers', isAuthenticatedUser, subscriptionController.getCreatorSubscriptions);
 
 // Check if user is subscribed to creator (protected route)
 router.get('/check/:creatorId', isAuthenticatedUser, subscriptionController.checkSubscription);
 
 // Subscribe to creator (protected route)
 router.post('/subscribe', isAuthenticatedUser, subscriptionController.subscribe);
+
+// Cancel subscription (protected route)
+router.put('/cancel/:subscriptionId', isAuthenticatedUser, subscriptionController.cancelSubscription);
+
+// Get subscription earnings (protected route for creators)
+router.get('/earnings', isAuthenticatedUser, subscriptionController.getSubscriptionEarnings);
+
+// Process subscription payment (webhook route)
+router.post('/process-payment', subscriptionController.processPayment);
 
 module.exports = router;
